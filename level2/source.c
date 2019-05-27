bc0bcf6c6adbb2c43f24b8d5d407f1ee7627ccff
Francois MAURY <fmaury@e2r5p10.42.fr>
2019-05-27 16:09:18 +0200

    level2

---
+++ level2/source.c
@@ -0,0 +1,22 @@
+
+
+int p()
+{
+	char buf[80];
+	unsigned int ret;
+
+	gets(buf);
+	ret = __builtin_return_address(0);
+
+  if((ret & 0xbf000000) == 0xbf000000) {
+    printf("(%p)\n", ret);
+    _exit(1);
+  }
+  puts(buf);
+}
+
+int main()
+{
+	p();
+  return (0);
+}
