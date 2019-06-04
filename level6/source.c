d4fa94bef135428efaf88cb94de8e8fa4e4a831b
cbarbier <cbarbier@student.42.fr>
2019-06-04 15:13:21 +0200

    kvl6 rework

--- level6/source.c
+++ level6/source.c
@@ -9,9 +9,10 @@ int main(int ac, char **av)
 	char *s1;
 	char *s2;
 
-	s1 = malloc(4);
 	s2 = malloc(64);
+	s1 = malloc(4);
+	*s1 = 0x8048468;
 	strcpy(s2, av[1]);
-	m();
+	(*s1)();
 	return (0);
 }
