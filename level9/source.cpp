f5a9901934e9e2ac73c31788bd2c564945489148
Francois MAURY <fmaury@e3r4p9.42.fr>
2019-06-06 11:58:03 +0200

    repush level9

--- level9/source.cpp
+++ level9/source.cpp
@@ -10,13 +10,13 @@ void N::setAnnotation(char* str)
 
 	len = std::strlen(str);
 	memcpy(this.str, str, len);
-
 }
 
 int main(int ac, char **av)
 {
 	if (ac < 2)
 		std::exit(1);
-	N *N = new N(5);
-	N.setAnnotation(av[1]);
+	N *N1 = new N(5);
+	N *N2 = new N(6);
+	N1.setAnnotation(av[1]);
 }
