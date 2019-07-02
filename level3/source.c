95bd93fcf9990a68c0563658a030723afcdf7d10
Cedric BARBIER <cbarbier@e1r4p13.42.fr>
2019-07-02 13:27:23 +0200

    source lvl3

--- level3/source.c
+++ level3/source.c
@@ -1,12 +1,16 @@
 
 void v()
 {
-	int c = 0;
+	char buf[512];
+	int c = 0; // 0804988c
 
-	fget(buf,size,stream);
+	fget(buf,512,STDIN);
 	printf(buf);
 	if (c == 64)
+	{
+		fwrite("Wait what?!\n", 1, 12, STDOUT);
 		system("/bin/sh");
+	}
 }
 
 int main()
